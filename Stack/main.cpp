3a79a202b2bd649909a12478cac20778348f24b5
namig17333 <c9:f4:67:a9:36:82:b7:25:6b:da:7b:43:0b:4e:4a:01>
2018-01-05 21:25:22 +0300

    Initial Commit

---
+++ Stack/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Stack
+//
+//  Created by Намиг on 05.01.18.
+//  Copyright © 2018 Намиг. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
